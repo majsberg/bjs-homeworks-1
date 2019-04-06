02ced42d2e64622cb1b3672fc81248dba898367e
Dmitry Kharlamov <majsberg@yandex.ru>
2019-04-06 17:25:16 +0300

    Вторая задача

--- variables/main.js
+++ variables/main.js
@@ -1,6 +1,9 @@
-let x = 2;
-let y = 22;
-let z = 0;
-let result;
-result = x * y + 5 * z + x - 1;
-console.log(result);
+let algebra;
+let geography;
+let physics;
+algebra = 5;
+geography = 4;
+physics = 4;
+let average;
+average = (algebra + physics + geography) / 3;
+console.log(average);
