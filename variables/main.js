612481ccb300ce3b286b6e28e2a0f802a276d4b7
Dmitry Kharlamov <majsberg@yandex.ru>
2019-04-06 17:39:08 +0300

    Третья задача

--- variables/main.js
+++ variables/main.js
@@ -1,9 +1,3 @@
-let algebra;
-let geography;
-let physics;
-algebra = 5;
-geography = 4;
-physics = 4;
-let average;
-average = (algebra + physics + geography) / 3;
-console.log(average);
+let name = 'Дима';
+let message = `Привет, мир! Меня зовут ${name}.`;
+console.log(message);
